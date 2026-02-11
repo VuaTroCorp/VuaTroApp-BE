c48384de576c0ea79058a153176c2b6063f2a986
Nguyễn Bùi Quang Huy <2k4quanghuy@gmail.com>
2026-02-11 15:31:23 +0700

    Fix

--- src/main/java/fpt/ntu/vuatrovn/config/SecurityConfig.java
+++ src/main/java/fpt/ntu/vuatrovn/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package fpt.ntu.vuatrovn.config;
 
-import fpt.ntu.vuatrovn.service.CustomOAuth2UserService;
-
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -11,51 +9,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;  
-import org.springframework.web.filter.CorsFilter;  
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(
-            HttpSecurity http,
-            CustomOAuth2UserService oAuth2UserService
-    ) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-    .cors(cors -> {})
-    .csrf(csrf -> csrf.disable())
+            .cors(cors -> {})
+            .csrf(csrf -> csrf.disable())
 
-    .authorizeHttpRequests(auth -> auth
-    .requestMatchers(
-        "/login/**",
-        "/swagger-ui/**",
-        "/v3/api-docs/**",
-        "/h2-console/**",
-        "/swagger-ui.html",
-        "/api/auth/**"
-    ).permitAll()
-    .anyRequest().authenticated()
-    )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/login/**",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/h2-console/**",
+                    "/swagger-ui.html",
+                    "/api/auth/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
 
-    // 👉 dùng login mặc định
-    .formLogin(form -> form
-        .loginPage("/login")
-        .permitAll()
-    )
+            // ✅ default login page của Spring
+            .formLogin(withDefaults())
 
-    .logout(logout -> logout
-        .logoutSuccessUrl("/")
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
-    )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+            )
 
-    .headers(headers ->
-        headers.frameOptions(frame -> frame.disable())
-    );
-
+            .headers(headers ->
+                headers.frameOptions(frame -> frame.disable())
+            );
 
         return http.build();
     }
