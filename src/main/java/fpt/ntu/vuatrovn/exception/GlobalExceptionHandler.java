64aa5bb7ee074634926b76a65ce7a293a573ffcc
Nguyễn Bùi Quang Huy <2k4quanghuy@gmail.com>
2026-02-27 16:40:09 +0700

    Fix

--- src/main/java/fpt/ntu/vuatrovn/exception/GlobalExceptionHandler.java
+++ src/main/java/fpt/ntu/vuatrovn/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package fpt.ntu.vuatrovn.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,20 +10,30 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<?> handleResponseStatusException(
-            ResponseStatusException ex){
-                        return ResponseEntity
+            ResponseStatusException ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", ex.getStatusCode().value());
+        error.put("message", ex.getReason());
+
+        return ResponseEntity
                 .status(ex.getStatusCode())
-                .body(ex.getReason());
-            }
-    
+                .body(error);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> handleRuntimeException(
             RuntimeException ex) {
 
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", 400);
+        error.put("message", ex.getMessage());
+
         return ResponseEntity
                 .badRequest()
-                .body(ex.getMessage());
+                .body(error);
     }
 }
